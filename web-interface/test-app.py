caac210bc5dc33544403520ed93c6c1c8aa8d485
Josh McCulloch <mcculloch@windowslive.com>
2021-05-03 16:51:59 +1200

    file saved to folder with name set to escaped original file path.

--- web-interface/test-app.py
+++ web-interface/test-app.py
@@ -1,5 +1,19 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
+from os.path import isfile, isdir, join, dirname, basename, abspath
+import pathlib
+
+def mkdir(directory):
+    print('creating dir "%s"' %directory)
+    pathlib.Path(directory).mkdir(parents=True, exist_ok=True)
+
+def save_file(file_path, file):
+    file_directory = abspath(join('./upload/', secure_filename(dirname(file_path))))
+    file_name = basename(file_path)
+    mkdir(file_directory)
+    print(file_path)
+    print("saving to \"%s\"" %join(file_directory, file_name))
+    file.save(join(file_directory, file_name))
 
 app = Flask(__name__)
 
@@ -15,10 +29,11 @@ def upload(projectid=None):
 @app.route('/upload_image', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        if "fullPath" in request.form:
-            print(request.form["fullPath"])
         f = request.files['image']
-        f.save('./upload/'+secure_filename(f.filename))
+        file_path = f.filename
+        if "fullPath" in request.form:
+            file_path = request.form["fullPath"]
+        save_file(file_path, f)
         return 'thanks'
     else:
         return "whoops"
